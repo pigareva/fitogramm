10c2ae0f543e36071bbd70b549d11f5662f80433
Olga Pigareva <olga@pigareva.de>
2018-07-12 14:59:50 +0200

    create item layout

--- src/containers/Item.jsx
+++ src/containers/Item.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Media, Button } from 'reactstrap';
+import { Button } from 'reactstrap';
 
 export default class Item extends React.Component {
   constructor(props) {
@@ -12,30 +12,33 @@ export default class Item extends React.Component {
       backgroundImage: `url(${this.props.classItem.eventGroup.imageUrl})`
     };
     return (
-      <Media className="item-box">
-        <Media left href="#">
-          <div style={ backgroundStyle }>{this.props.classItem.name}</div>
-        </Media>
-        <Media body>
-          <div className="info-box">
-            <div className="time-box">
-              <p>{this.props.classItem.startDateTime}</p>
-              {
-                this.props.classItem.bookingWindowStartHours &&
-                <p>{this.props.classItem.bookingWindowStartHours}</p>
-              }
-            </div>
-            <div className="name-box">{this.props.classItem.eventGroup.name}</div>
-            <div className="location-box">{this.props.classItem.location.city}</div>
-            <div className="trainer-box">{this.props.classItem.trainers.map(trainer => <p key={trainer.id}>{trainer.name}</p>)}</div>
-            <div className="description-box">{this.props.classItem.descriptions.map(description => <p key={description.id}>{description.text}</p>)}</div>
+      <div className="item-container">
+
+        <div style={backgroundStyle} className="item-box image-box">
+          <div>{this.props.classItem.name}</div>
+        </div>
+
+        <div className="item-box info-box">
+          <div className="time-box">
+            <p>{this.props.classItem.startDateTime}</p>
+            {
+              this.props.classItem.bookingWindowStartHours &&
+              <p>{this.props.classItem.bookingWindowStartHours}</p>
+            }
           </div>
-          <div className="info-box">
-            <Button color="warning">Zur Buchung</Button>
-            <div className="description-box">{this.props.classItem.endDateTime}</div>
-          </div>
-        </Media>
-      </Media>
+          <div className="name-box">{this.props.classItem.eventGroup.name}</div>
+          <div className="location-box">{this.props.classItem.location.city}</div>
+          <div className="trainer-box">{this.props.classItem.trainers.map(trainer => <p
+            key={trainer.id}>{trainer.name}</p>)}</div>
+          <div className="description-box">{this.props.classItem.descriptions.map(description => description.text)}</div>
+        </div>
+
+        <div className="item-box booking-box">
+          <Button color="warning">Zur Buchung</Button>
+          <div className="description-box">{this.props.classItem.endDateTime}</div>
+        </div>
+
+      </div>
     )
   }
 }
