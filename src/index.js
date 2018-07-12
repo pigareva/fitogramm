400850dd5f1583c9ac8756527614f928ee95bde4
Olga Pigareva <olga@pigareva.de>
2018-07-12 14:03:38 +0200

    add ErrorBoundary

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ErrorBoundary from './components/ErrorBoundary'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-
+  <ErrorBoundary>
     <App/>
-
+  </ErrorBoundary>
   , document.getElementById('root'));
 registerServiceWorker();
