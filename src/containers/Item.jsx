960b7bc5093c63846f07dd7d68f593e26b91ad43
Olga Pigareva <olga@pigareva.de>
2018-07-12 16:25:10 +0200

    implement moment js

--- src/containers/Item.jsx
+++ src/containers/Item.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
+import * as moment from 'moment';
+import 'moment/locale/de';
 import { Button, Container, Row, Col } from 'reactstrap';
 
 export default class Item extends React.Component {
   constructor(props) {
     super(props);
+    this.createMarkup = this.createMarkup.bind(this);
+  }
+
+  createMarkup() {
+    return { __html: this.props.classItem.descriptions.map(description => description.text) };
   }
 
   render() {
     const backgroundStyle = {
-      backgroundImage: `url(${this.props.classItem.eventGroup.imageUrl})`
+      backgroundImage: `url(${this.props.classItem.eventGroup.imageUrl})`,
     };
     return (
       <div className="item-container">
@@ -24,11 +31,11 @@ export default class Item extends React.Component {
               <Col xs="6">
                 <Row>
                   <Col xs="12">
-                    {this.props.classItem.startDateTime}
+                    {moment(this.props.classItem.startDateTime).format("dd DD MMM YYYY")}
                   </Col>
                   {
                     this.props.classItem.bookingWindowStartHours &&
-                    <Col xs="12">{this.props.classItem.bookingWindowStartHours}</Col>
+                    <Col xs="12">{moment(this.props.classItem.bookingWindowStartHours).format("HH mm")}</Col>
                   }
                 </Row>
               </Col>
@@ -45,17 +52,17 @@ export default class Item extends React.Component {
                   key={trainer.id}>{trainer.name}</p>)}
               </Col>
             </Row>
-            <Row>
-              <Col xs="12">
-                <div className="description-box">{this.props.classItem.descriptions.map(description => description.text)}</div>
-              </Col>
-            </Row>
+            <div className="description-box" dangerouslySetInnerHTML={this.createMarkup()}>
+            </div>
           </Container>
         </div>
 
         <div className="item-box booking-box">
           <Button color="warning">Zur Buchung</Button>
-          <div className="description-box">{this.props.classItem.endDateTime}</div>
+          <div className="description-box">
+            <p>Buchung bis:</p>
+            <p>{moment(this.props.classItem.endDateTime).format("dd DD MMM YYYY")}</p>
+          </div>
         </div>
 
       </div>
